3fcbf18019b25cd62a090164c4bf691115a9e5a3
Ritesh <ritesh.nus@gmail.com>
2018-08-01 11:49:07 +0800

    URL modified

--- Wego/urls.py
+++ Wego/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('coffeeshop/', include('coffeeshop.urls')),
+    path('', include('coffeeshop.urls')),
     path('admin/', admin.site.urls),
 ]
