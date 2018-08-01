de5e9494980153482399db603e4cf43cf921b998
Ritesh <ritesh.nus@gmail.com>
2018-08-01 12:31:42 +0800

    update the gunicorn dependency

--- wego/settings.py
+++ wego/settings.py
@@ -133,5 +133,5 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_DIR, 'static'),
 )
